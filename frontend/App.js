1deec5e9def8c53d4b96c8473ce4ec23971640a9
Wilopo <wilopo@Wilopos-MacBook-Pro.local>
2022-11-06 21:10:19 +0700

    Initial Commit

---
+++ frontend/App.js
@@ -0,0 +1,45 @@
+import 'regenerator-runtime/runtime';
+import React from 'react';
+
+import './assets/global.css';
+
+import { AddPost, AllPosts, PostsByTag, SignInPrompt, SignOutButton } from './ui-components';
+
+
+export default function App({ isSignedIn, contract, wallet }) {
+  const [valueFromBlockchain, setValueFromBlockchain] = React.useState();
+  const [uiPleaseWait, setUiPleaseWait] = React.useState(true);
+  const [allPosts, setAllPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    contract.get_all_posts()
+      .then(setAllPosts)
+      .catch(alert)
+      .finally(()=> {
+        setUiPleaseWait(false);
+      });
+  },[]);
+
+  /// If user not signed-in with wallet - show prompt
+  if (!isSignedIn) {
+    // Sign-in flow will reload the page later
+    return <SignInPrompt greeting={valueFromBlockchain} onClick={() => wallet.signIn()}/>;
+  }
+
+
+  return (
+    <>
+      <SignOutButton accountId={wallet.accountId} onClick={() => wallet.signOut()}/>
+      <main className={uiPleaseWait ? 'please-wait' : ''}>
+        <h1>Social Media NEAR Dapp</h1>   
+        <PostsByTag />
+
+        <AllPosts allPosts={allPosts} setUiPleaseWait={setUiPleaseWait} 
+            setAllPosts={setAllPosts} contract={contract}/>
+
+        <AddPost contract={contract} seeAllPosts={setAllPosts}/>
+        <br/> <br/>
+      </main>
+    </>
+  );
+}
