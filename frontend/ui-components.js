d953e432c476181c3a503ccd64840d7060168388
Wilopo <wilopo@192.168.209.39>
2022-11-20 23:36:13 +0700

    change into bootstrap design

--- frontend/ui-components.js
+++ frontend/ui-components.js
@@ -94,20 +94,38 @@ export function Post({ post, setUiPleaseWait, setAllPosts, contract}) {
 
 export function SignOutButton({accountId, onClick}) {
   return (
-    <button style={{ float: 'right' }} onClick={onClick}>
+    <button style={{ float: 'right' }} onClick={onClick} className="btn btn-sm btn-outline-secondary">
       Sign out {accountId}
     </button>
   );
 }
 
-export function PostsByTag({}){
+export function PostsByTag({ setUiPleaseWait, contract, setAllPosts }){
+
+  const [ postByTag, setPostByTag] = React.useState([]);
+function get_posts_by_tag(e){
+  e.preventDefault();
+  setUiPleaseWait(true);
+
+  var tag = document.getElementById("tag").value;
+
+  contract.get_posts_by_tag(tag)
+    .then(setPostByTag)
+    .finally( () =>{
+      setUiPleaseWait(false);
+    });
+}
+
   return (
     <>
     <h2>Get Posts By Tag</h2>
-    <form>
+    <form onSubmit={get_posts_by_tag}>
       <label>Tag</label><br/>
       <input placeholder='tag' id='tag'/>
       <button>Get Posts By Tag</button>
+      <AllPosts allPosts={postByTag} setUiPleaseWait={setUiPleaseWait} 
+        setAllPosts={setAllPosts} contract={contract}
+      />
     </form>
     </>
   )
@@ -120,9 +138,12 @@ export function AllPosts({ allPosts, setUiPleaseWait, setAllPosts, contract }){
     { 
     
       allPosts ? allPosts.map((post,index) =>{
+        if(post[1]){
+          post = post[1]
+        }
         return (
           <div key={index}>
-            <Post key={index} post={post[1]} setUiPleaseWait={setUiPleaseWait} 
+            <Post post={post} setUiPleaseWait={setUiPleaseWait} 
             setAllPosts={setAllPosts} contract={contract}/>
           </div> 
         )
